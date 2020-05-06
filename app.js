1cf579db2ced0e288aba0349ef39b5cdacd613d3
sameh11 <sameh.hamza11@gmail.com>
2020-05-06 07:03:52 +0200

    update - heroku depolyment

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ app.use(function (err, req, res, next) {
 });
 
 app.use(function (req, res, next) {
-    next(createError(404));
+    // next(createError(404));
 });
 
 app.use(function (err, req, res, next) {
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
     // res.status(err.status || 500);
     // res.send('error');
 });
